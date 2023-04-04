1e6b19c583448b56e3bb4ca899ec5d63273c8d53
Ethan Berk <Ethanwberk@gmail.com>
2023-04-04 15:25:34 -0600

    AutoMarker

--- src/main/java/frc/robot/commands/Auto/AutoEventMarkers.java
+++ src/main/java/frc/robot/commands/Auto/AutoEventMarkers.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import frc.robot.commands.objectManipulation.intake.IntakeCommandFactory;
+import frc.robot.commands.superStructure.IntakePositionCommandFactory;
 import frc.robot.commands.superStructure.IntakeToPosition;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
@@ -20,8 +21,8 @@ public class AutoEventMarkers {
     public static Map<String, Command> getMap(Intake intakeAndShooter, Elevator elevator, Wrist wrist) {
         return Map.of(
             "IntakeCube", IntakeCommandFactory.intakeCube(wrist, elevator, intakeAndShooter),
-
-            "IntakeStandingCone", IntakeToPosition.home(wrist, elevator).withInterruptBehavior(InterruptionBehavior.kCancelSelf),
+            "IntakeToHighCube", IntakePositionCommandFactory.IntakeToHighCubeNode(elevator, wrist),
+            "IntakeToHybrid", IntakePositionCommandFactory.IntakeToStandingConeNode(elevator, wrist),
             "Home", IntakeToPosition.home(wrist, elevator).withInterruptBehavior(InterruptionBehavior.kCancelSelf)
             );
     }
